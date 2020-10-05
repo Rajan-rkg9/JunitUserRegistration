8b3849c2406f96512402a421015b9d7e4d0142d5
Rajan-rkg9 <amirajangupta@gmail.com>
2020-10-05 17:28:21 +0530

    [Rajan-rkg9] Test. Multiple Emails Checked using Parameteried Test

---
+++ test/java/com/capg/userregistration/MultipleEmailsTest.java
@@ -0,0 +1,41 @@
+package com.capg.userregistration;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
+public class MultipleEmailsTest {
+	private String emailId;
+	private String outcome;
+	private UserRegistration multipleTest; 
+	
+
+	public MultipleEmailsTest(String emailId, String outcome) {
+		super();
+		this.emailId = emailId;
+		this.outcome = outcome;
+	}
+	 
+	@Before
+	public void initialize() {
+		multipleTest = new UserRegistration();
+	}
+
+	@Parameterized.Parameters
+	public static Collection input() {
+		String array[][] = {{"abc@yahoo.com","true"},{"abc-100@yahoo.com","true"},{"abc.100@yahoo.com","true"},{"abc111@abc.com","true"},{"abc-100@abc.net","true"},{"abc.100@abc.com.au","true"},{"abc@1.com","true"},{"abc@gmail.com.com","true"},{"abc+100@gmail.com","true"},{"abc","false"},{"abc@.com.my","false"},{"abc123@gmail.a","false"},{"abc123@.com","false"},{"abc123@.com.com","false"},{".abc@abc.com","false"},{"abc()*@gmail.com","false"},{"abc@%*.com","false"},{"abc..2002@gmail.com","false"},{"abc.@gmail.com","false"},{"abc@abc@gmail.com","false"},{"abc@gmail.com.1a","false"},{"abc@gmail.com.aa.au","false"}};
+		return Arrays.asList(array);
+	}
+	@Test
+	public void testMultipleEmailsTest() {
+		assertEquals("Check For Correct Email Id", Boolean.parseBoolean(outcome), multipleTest.validateEmailId(emailId));
+	}
+
+}
